e02c8c2792743bb7324a3ee5dffa7ba157e14d69
yocool <544301172@qq.com>
2016-07-04 15:24:42 +0800

    添加404错误处理

--- springboot_web/src/main/java/com/yocool/controller/web/AppErrorController.java
+++ springboot_web/src/main/java/com/yocool/controller/web/AppErrorController.java
@@ -1,7 +1,9 @@
 package com.yocool.controller.web;
 
+import org.slf4j.Logger;
 import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * 错误处理控制器
@@ -10,9 +12,17 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class AppErrorController implements ErrorController {
 
-    //TODO 错误处理
+    private static final Logger logger = org.slf4j.LoggerFactory.getLogger(AppErrorController.class);
+
+    private static final String ERROR_PATH = "/error";
+
+    @RequestMapping(value=ERROR_PATH)
+    public String handleError(){
+        return "error/404";
+    }
+
     @Override
     public String getErrorPath() {
-        return "/error";
+        return ERROR_PATH;
     }
 }

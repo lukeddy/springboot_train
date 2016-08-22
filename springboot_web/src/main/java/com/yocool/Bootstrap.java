6c3ea0014cef092577b72dc4c1a168897decba74
yocool <544301172@qq.com>
2016-08-22 14:27:43 +0800

    查看springboot管理的所有的been

--- springboot_web/src/main/java/com/yocool/Bootstrap.java
+++ springboot_web/src/main/java/com/yocool/Bootstrap.java
@@ -2,10 +2,20 @@ package com.yocool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication(scanBasePackages = "com.yocool")
 public class Bootstrap {
 	public static void main(String[] args) {
-		SpringApplication.run(Bootstrap.class, args);
+		ApplicationContext ctx =SpringApplication.run(Bootstrap.class, args);
+
+		System.out.println("看看Spring Boot提供了哪些been:");
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for (String beanName : beanNames) {
+			System.out.println(beanName);
+		}
 	}
 }

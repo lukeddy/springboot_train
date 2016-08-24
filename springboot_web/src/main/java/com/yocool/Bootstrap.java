e58df010776896448e783111d715133ee3f4981d
yocool <544301172@qq.com>
2016-08-24 19:19:23 +0800

    使用actuator库查看服务状态，如/mappings可以查看所有列表

--- springboot_web/src/main/java/com/yocool/Bootstrap.java
+++ springboot_web/src/main/java/com/yocool/Bootstrap.java
@@ -11,11 +11,11 @@ public class Bootstrap {
 	public static void main(String[] args) {
 		ApplicationContext ctx =SpringApplication.run(Bootstrap.class, args);
 
-		System.out.println("看看Spring Boot提供了哪些been:");
-		String[] beanNames = ctx.getBeanDefinitionNames();
-		Arrays.sort(beanNames);
-		for (String beanName : beanNames) {
-			System.out.println(beanName);
-		}
+//		System.out.println("看看Spring Boot提供了哪些been:");
+//		String[] beanNames = ctx.getBeanDefinitionNames();
+//		Arrays.sort(beanNames);
+//		for (String beanName : beanNames) {
+//			System.out.println(beanName);
+//		}
 	}
 }

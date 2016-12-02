753c341d2e3c3deb92a028f94f8b70b1a5f71342
yocool <544301172@qq.com>
2016-12-02 15:25:14 +0800

    使用代码执行数据库脚本

--- springboot-mybatis-postgresql-flyway/src/main/java/com/tangzq/BootstrapHelper.java
+++ springboot-mybatis-postgresql-flyway/src/main/java/com/tangzq/BootstrapHelper.java
@@ -1,6 +1,7 @@
 package com.tangzq;
 
 import com.tangzq.config.GlobalConfig;
+import org.flywaydb.core.Flyway;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+
+import java.util.ResourceBundle;
 
 /**
  * 项目启动时做一些初始化工作，如数据初始化
@@ -29,22 +32,22 @@ public class BootstrapHelper implements ApplicationListener<ContextRefreshedEven
       logger.info("测试配置1："+globalConfig.getTestURL());
       logger.info("测试配置2："+globalConfig.getEnv());
       //在这里初始化数据库
-      //initDatabase();
+      initDatabase();
       logger.info("项目启动初始化工作完成！Great!");
     }
 
     /**
      * 使用Flyway初始换数据库
      */
-//    private void initDatabase(){
-//        ResourceBundle applicationProperties = ResourceBundle.getBundle("application");
-//        Flyway flyway = new Flyway();
-//        flyway.setDataSource(
-//                applicationProperties.getString("spring.datasource.url"),
-//                applicationProperties.getString("spring.datasource.username"),
-//                applicationProperties.getString("spring.datasource.password"));
-//        flyway.setBaselineOnMigrate(true);
-//        flyway.migrate();
-//        logger.info("初始换数据库完成！");
-//    }
+    private void initDatabase(){
+        ResourceBundle applicationProperties = ResourceBundle.getBundle("application");
+        Flyway flyway = new Flyway();
+        flyway.setDataSource(
+                applicationProperties.getString("spring.datasource.url"),
+                applicationProperties.getString("spring.datasource.username"),
+                applicationProperties.getString("spring.datasource.password"));
+        flyway.setBaselineOnMigrate(true);
+        flyway.migrate();
+        logger.info("初始换数据库完成！");
+    }
 }

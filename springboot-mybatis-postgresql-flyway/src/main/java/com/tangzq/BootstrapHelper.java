d07cd982e9fe2f0ecf7aaecf0c88db1f9c0ad188
yocool <544301172@qq.com>
2016-12-05 15:35:23 +0800

    使用java代码实现数据库migration

--- springboot-mybatis-postgresql-flyway/src/main/java/com/tangzq/BootstrapHelper.java
+++ springboot-mybatis-postgresql-flyway/src/main/java/com/tangzq/BootstrapHelper.java
@@ -5,24 +5,27 @@ import org.flywaydb.core.Flyway;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.flyway.FlywayProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
-
-import java.util.ResourceBundle;
+import org.thymeleaf.util.StringUtils;
 
 /**
  * 项目启动时做一些初始化工作，如数据初始化
  */
 @Component
-@EnableConfigurationProperties(GlobalConfig.class)
+@EnableConfigurationProperties(value = {GlobalConfig.class, FlywayProperties.class})
 public class BootstrapHelper implements ApplicationListener<ContextRefreshedEvent> {
 
     private static final Logger logger = LoggerFactory.getLogger(BootstrapHelper.class);
 
     @Autowired
     private GlobalConfig globalConfig;
+
+    @Autowired
+    private FlywayProperties flywayProperties;
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
@@ -31,7 +34,26 @@ public class BootstrapHelper implements ApplicationListener<ContextRefreshedEven
       logger.info("环境："+globalConfig.getEnv());
       logger.info("测试配置1："+globalConfig.getTestURL());
       logger.info("测试配置2："+globalConfig.getEnv());
+      initDatabase();
       logger.info("项目启动初始化工作完成！Great!");
     }
 
+    /**
+     * 使用Flyway初始换数据库
+     */
+    private void initDatabase(){
+        Flyway flyway = new Flyway();
+        flyway.setLocations(StringUtils.join(flywayProperties.getLocations(),","));
+        flyway.setDataSource(
+                flywayProperties.getUrl(),
+                flywayProperties.getUser(),
+                flywayProperties.getPassword());
+        flyway.setBaselineOnMigrate(true);
+        logger.warn("开始清除数据表信息");
+        flyway.clean();
+        logger.warn("清除完成！");
+        logger.info("重新创建数据表信息");
+        flyway.migrate();
+        logger.info("初始换数据库表和数据完成！");
+    }
 }
---
+++ springboot-mybatis-postgresql-flyway/src/main/resources/db/migration/postgres/V1_4_1__Add_new_data.sql
@@ -0,0 +1,3 @@
+update sysuser set country='中国成都';
+insert into athletes (id,first_name,last_name) VALUES (5,'小改','吕');
+
